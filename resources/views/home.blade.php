9e9246391baabce14b8330a87969f81e1eb967c2
irkuhs <shukriahmad56@gmail.com>
2023-03-16 10:54:32 +0800

    add: inventory list

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,46 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+
+            <div class="card mt-2">
+                <div class="card-header">Inventory</div>
+
+                <div class="card-body">
+
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col"></th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                          </tr>
+                          <tr>
+                            <th scope="row">3</th>
+                            <td colspan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                    <button type="button" class="btn btn-primary">Tambah lain</button>
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
