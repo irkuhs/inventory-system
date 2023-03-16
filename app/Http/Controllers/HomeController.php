f6d7df967e80ed77c3512930bbf79468622db58d
irkuhs <shukriahmad56@gmail.com>
2023-03-16 13:53:04 +0800

    add: read data and show in table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inventori;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $inventories = Inventori::all();
+        return view('home', compact('inventories'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,31 +25,21 @@
 
                     <table class="table">
                         <thead>
-                          <tr>
-                            <th scope="col"></th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                          </tr>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Action</th>
+                            </tr>
                         </thead>
                         <tbody>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">3</th>
-                            <td colspan="2">Larry the Bird</td>
-                            <td>@twitter</td>
-                          </tr>
+                            @foreach ( $inventories as $key => $inventory)
+                            <tr>
+                                <th scope="row">{{ $key+1}}</th>
+                                <td>{{ $inventory->name}}</td>
+                                <td>{{ $inventory->description}}</td>
+                            </tr>
+                            @endforeach
                         </tbody>
                       </table>
 
